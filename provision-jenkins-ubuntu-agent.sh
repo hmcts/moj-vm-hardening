2f04c848ac226aed65abd77f4b8af7bf30d56139
Enda Kelly <enda.kelly@hmcts.net>
2021-11-17 12:57:42 +0000

    Updates to docker in script

--- provision-jenkins-ubuntu-agent.sh
+++ provision-jenkins-ubuntu-agent.sh
@@ -13,18 +13,19 @@ pip3 install --upgrade docker-compose
 curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
 apt install -y nodejs
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 npm install npm@latest minimatch@latest graceful-fs@latest -g
 npm install --global gulp eslint
 
 npm install --global yarn
 
-apt remove azure-cli -y && apt autoremove -y
-curl -sL https://aka.ms/InstallAzureCLIDeb
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt remove docker docker-engine docker.io containerd runc -y && apt autoremove -y
+apt-get update
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 
 apt install -y \
   ca-certificates \
