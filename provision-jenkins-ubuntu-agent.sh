d623c6f306cb149f0a8763e497d07987af315128
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-01-05 15:41:45 +0000

    fail script on purpose

--- provision-jenkins-ubuntu-agent.sh
+++ provision-jenkins-ubuntu-agent.sh
@@ -2,6 +2,8 @@
 
 set -xe
 
+exit 1
+
 echo '-----BEGIN RSA PRIVATE KEY-----' | tee /opt/jenkinsssh_id_rsa
 echo $JENKINS_SSH_KEY | sed -e 's/[[:blank:]]\\+/\\n/g' | tee -a /opt/jenkinsssh_id_rsa
 echo '-----END RSA PRIVATE KEY-----' | tee -a /opt/jenkinsssh_id_rsa
