87abe453f90e26e036d3e9943bf748ea40bae317
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-10-31 10:20:25 +0000

    Switch node install to new method

--- provision-jenkins-ubuntu-agent.sh
+++ provision-jenkins-ubuntu-agent.sh
@@ -62,10 +62,8 @@ apt-cache policy python3-pip
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
-apt purge nodejs -y
-rm -rf /etc/apt/sources.list.d/nodesource.list
-
-curl -fsSL https://deb.nodesource.com/setup_${NODE_VERSION}.x | bash
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_VERSION.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 apt update
 apt install -y nodejs
