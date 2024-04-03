091e6240891e5816daf46b34d3b4ed8cafc4a3cf
Marty Fox <martin.fox1@justice.gov.uk>
2024-04-03 11:44:01 +0100

    Adding ffmpeg to allow teams to do audio transcoding

--- provision-jenkins-ubuntu-agent.sh
+++ provision-jenkins-ubuntu-agent.sh
@@ -153,7 +153,8 @@ apt install -y \
   libncurses-dev \
   pdftk-java \
   libreoffice-core \
-  libreoffice-writer 
+  libreoffice-writer \
+  ffmpeg
 
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
