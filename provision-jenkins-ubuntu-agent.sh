73941e7b99bc86464eeb7a3fad0ebd447c6dc7fd
Tony Chow <36265570+tchow8@users.noreply.github.com>
2024-10-15 11:06:36 +0100

    Add MS Repository first before installing mssql-tools with apt

--- provision-jenkins-ubuntu-agent.sh
+++ provision-jenkins-ubuntu-agent.sh
@@ -67,6 +67,9 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key a
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_VERSION.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+curl "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list" | sudo tee /etc/apt/sources.list.d/mssql-release.list
+
 apt update
 apt install -y nodejs
 
